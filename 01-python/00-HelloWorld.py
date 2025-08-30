c72ee52260bd2645dfc34070d814941fe5eecd00
Fabricio Santana <ffsant@senado.leg.br>
2025-08-30 12:08:46 +0000

    hello

---
+++ 01-python/00-HelloWorld.py
@@ -0,0 +1,10 @@
+print("Hello, World!")
+
+def main(name):
+    print("Hello, " + name + "!")
+
+main("Fabricio")
+
+name = input("Digite o seu nome: ")
+
+print("Hello, " + name + "!" )
--- 01-python/HelloWorld.py
+++
@@ -1,6 +0,0 @@
-print("Hello, World!")
-
-def main(name):
-    print("Hello, " + name + "!")
-
-main("Fabricio")
